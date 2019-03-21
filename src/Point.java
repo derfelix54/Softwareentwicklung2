168ef1b7b2e84e37fce4352005d8c70f9e885486
felix <reichlin@hm.edu>
2019-03-21 08:33:01 +0100

    merge

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public interface Point {
+
+    abstract double getxCoord();
+    abstract double getyCoord();
+    abstract double getDistance();
+    abstract double getAngle();
+    abstract boolean closeTo(Point other);
 }
--- src/Polar.java
+++ src/Polar.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+public class Polar implements Point{
 
-public class Polar {
+    private double distance;
+    private double angle;
+
+    Polar(double dist, double angle){
+
+    }
+
+    @Override
+    public double getxCoord() {
+        return 0;
+    }
+
+    @Override
+    public double getyCoord() {
+        return 0;
+    }
+
+    @Override
+    public double getAngle() {
+        return angle;
+    }
+
+    @Override
+    public double getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean closeTo(Point other) {
+        return false;
+    }
 }
