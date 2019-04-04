aa5f1cd554063d4f56267dd8b5cbf23f233bfdc8
felix <reichlin@hm.edu>
2019-04-04 08:31:11 +0200

    spacedIntegeres von Praktikum

---
+++ src/spacedIntegers/Main.java
@@ -0,0 +1,13 @@
+package spacedIntegers;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		SpacedIntegers numbers = new SpacedIntegers(2);
+		System.out.println(numbers.add(10));	
+		
+		
+	}
+
+}
---
+++ src/spacedIntegers/SpacedIntegers.java
@@ -0,0 +1,61 @@
+package spacedIntegers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class SpacedIntegers extends HashSet<Integer>{
+	
+	HashSet<Integer> set;
+	private int minDistance;
+	
+	SpacedIntegers(int minDistance){
+		if(minDistance <= 0) {
+			throw new IllegalArgumentException();
+		}
+		set = new HashSet<Integer>();
+		
+	}
+	
+	public boolean add(Integer newElement) {
+		ArrayList<Integer> list = new ArrayList<Integer>(set);
+		Collections.sort(list); //sort hash set
+		boolean value = true;
+		if(list.isEmpty()) {
+			this.add(newElement);
+			value = true;
+		}
+		else if((Math.abs(list.get(0)) - newElement) < minDistance) {
+			value = false;
+		}else {
+			set.add(newElement);
+		}
+		
+		
+		return value;
+		
+	}
+	
+	public boolean addAll(Collection<? extends Integer> collection) {
+		
+		return this.addAll(collection);
+		
+		
+	}
+	
+	public int getCurrentMinDistance() {
+		List<Integer> list = new ArrayList<>(set);
+		Collections.sort(list); //sort hash set
+		//biggest numbers at the top
+		int res = list.get(0) - list.get(1);
+		return res;
+	}
+
+
+	
+	
+	
+
+}
